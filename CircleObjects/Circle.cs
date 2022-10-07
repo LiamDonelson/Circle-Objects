55e588dd16618d4c62be6dcabe9f80534ec475ad
unknown <liamsdonelson@gmail.com>
2022-10-07 13:54:47 -0400

    First Commit

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    internal class Circle
+    {
+        public Circle(double radius)
+        {
+
+
+
+
+        }
+
+        public string CalculateCircumference(double radius)
+        {
+            double calcualted = 2 * Math.PI * radius;
+            string formattedCircumference = "The calculated circumfrence is :" + Math.Round(calcualted, 2);
+
+            return formattedCircumference;
+
+        }
+
+        public string CalculateArea(double radius)
+        {
+            double calcualted = Math.PI * radius * radius;
+            string formattedArea = "The calcualted area of your circle is: " + Math.Round(calcualted,2);
+
+
+            return formattedArea;
+        }
+
+    }
+}
+
+
+
+//Build Specifications:
+//Create a class named Circle to store the data about this circle. This class should contain these constructors and methods:
+//public Circle(double radius)
+//public double CalculateCircumference()
+//public string CalculateFormattedCircumference()
+//public double CalculateArea()
+//public string CalculateFormattedArea()
+//private string FormatNumber(double x)
+//Define a member called radius. This member should be private. 
+//Define a property to get access to the class member : Radius
+//For the value of pi, use the PI constant of the System.Math class.
+//Get the user input, create a Circle object, and display the circumference and area.
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Runtime.Intrinsics.X86;
+
+namespace CircleObjects
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            while (true)
+            {
+                i++;
+
+                Console.WriteLine("Enter Radius:");
+                double radius = 0;
+                try
+                {
+                    radius = double.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("You have not enters a valid answer. Please try again with only numbers");
+                    i--;
+                }
+
+                Circle c = new Circle(radius);
+                c.CalculateCircumference(radius);
+
+                Console.WriteLine($"Circumfrence: {c.CalculateCircumference(radius)}");
+
+                Console.WriteLine($"Area: {c.CalculateArea(radius)}");
+
+
+
+
+                if (RunAgain() == false)
+                {
+                    Console.WriteLine("Number of circles made:" + i);
+                    break;
+                }
+
+            }
+        }
+
+        static bool RunAgain()
+        {
+            
+
+            Console.WriteLine("Would you like to enter another Radius? Y/N");
+            string restart = Console.ReadLine().ToLower();
+            if (restart == "y")
+            {
+                return true;
+            }
+            else if (restart == "n")
+            {
+                // ends the program
+                Console.WriteLine(" Have a great day !");
+                return false;
+            }
+            else
+            {
+                // runs the program again
+                Console.WriteLine(" Im sorry, im not sure what that meant.");
+                return RunAgain();
+            }
+
+        }
+    }
+}
